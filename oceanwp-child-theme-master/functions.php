d3eaa791640740ea3b2610e7f30fcde3eab8ec8d
florian <filin-florian@live.fr>
2024-06-27 10:59:07 +0200

    first commit

---
+++ oceanwp-child-theme-master/functions.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Child theme functions
+ *
+ * When using a child theme (see http://codex.wordpress.org/Theme_Development
+ * and http://codex.wordpress.org/Child_Themes), you can override certain
+ * functions (those wrapped in a function_exists() call) by defining them first
+ * in your child theme's functions.php file. The child theme's functions.php
+ * file is included before the parent theme's file, so the child theme
+ * functions would be used.
+ *
+ * Text Domain: oceanwp
+ * @link http://codex.wordpress.org/Plugin_API
+ *
+ */
+
+/**
+ * Load the parent style.css file
+ *
+ * @link http://codex.wordpress.org/Child_Themes
+ */
+function oceanwp_child_enqueue_parent_style() {
+	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update your theme)
+	$theme   = wp_get_theme( 'OceanWP' );
+	$version = $theme->get( 'Version' );
+	// Load the stylesheet
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'oceanwp-style' ), $version );
+
+}
+add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
+
+
+
+// Bonton de contat 
+function contact_btn( $items, $args ) {
+	$items .= '<a href="/contact" class="contact-btn">Nous contacter</a>';
+	return $items;
+}
+
+add_filter( 'wp_nav_menu_items', 'contact_btn', 10, 2 );
+
+
+// Appel de la Bibliothèque Jquery > popup
+function my_theme_enqueue_scripts() {
+	wp_deregister_script('jquery');
+	wp_register_script('jquery', 'https://code.jquery.com/jquery-3.6.0.min.js', false, '3.6.0');
+	wp_enqueue_script('jquery');
+}
+
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_scripts');
+?>
